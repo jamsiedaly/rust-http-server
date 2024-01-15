f16a755bd9ece63553ecb179434f88ee5d4150f4
James Daly <jamsiedaly@gmail.com>
2024-01-15 21:43:33 +0100

    Parsing the message and sending a response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
@@ -9,6 +9,20 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
+                let mut buf: [u8; 128] = [0; 128];
+                if let Ok(message_length) = stream.read(&mut buf) {
+                    let request = String::from_utf8_lossy(&buf[..message_length]);
+
+                    let message = parse_request(&request);
+
+                    if message.path == "/" {
+                        stream.write(b"HTTP/1.1 200 OK").unwrap();
+                    } else {
+                        stream.write(b"HTTP/1.1 404 NOT FOUND").unwrap();
+                    }
+
+                }
+
                 println!("accepted new connection");
                 stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
             }
@@ -17,4 +31,37 @@ fn main() {
             }
         }
     }
+}
+
+
+#[derive(Debug)]
+struct Message {
+    method: String,
+    headers: Vec<String>,
+    path: String,
+    http_version: String,
+}
+
+fn parse_request(request: &str) -> Message {
+    let mut lines = request.lines();
+    let first_line = lines.next().unwrap();
+    let mut parts = first_line.split_whitespace();
+    let method = parts.next().unwrap().to_owned();
+    let path = parts.next().unwrap().to_owned();
+    let http_version = parts.next().unwrap().to_owned();
+
+    let mut headers = Vec::new();
+    lines.into_iter().for_each(|line| {
+        if !line.is_empty() {
+            headers.push(line.to_string());
+        }
+    });
+    headers.remove(headers.len() - 1);
+
+    return Message {
+        method,
+        headers,
+        path,
+        http_version,
+    };
 }
